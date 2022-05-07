cdec614b56f03d05214b722f0820d546b244a10f
Ah-oy <58558581+ei-blue@users.noreply.github.com>
2022-05-06 22:17:22 -0700

    adding flipping cards with colors

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+const movies = {"Captain Marvel":"Peripheral","I Care a Lot":"Lesbian","Bohemian Rhapsody":"Gay","A Star Is Born":"Peripheral","Solo: A Star Wars Story":"Peripheral","Bird Box":"Peripheral","Birds of Prey":"Bisexual","Blade Runner 2049":"Peripheral","It Chapter Two":"Peripheral","Alien: Covenant":"Peripheral","Get Out":"Peripheral","Guardians of the Galaxy":"Peripheral","Deadpool":"Peripheral","Green Book":"Gay","The Old Guard":"Peripheral","Fantastic Beasts: The Crimes of Grindelwald":"Peripheral","Deadpool 2":"Peripheral","Beauty and the Beast":"Peripheral","The Shape of Water":"Gay","Guardians of the Galaxy Vol. 2":"Peripheral","The Favourite":"Lesbian","The Grand Budapest Hotel":"Peripheral","Ghost in the Shell":"Peripheral","Call Me by Your Name":"Gay","The New Mutants":"Peripheral","Moonlight":"Gay","Star Trek Beyond":"Peripheral","The Imitation Game":"Gay","Atomic Blonde":"Bisexual","The Lobster":"Peripheral","A Simple Favor":"Bisexual","Spotlight":"Gay","Ted":"Peripheral","The Perks of Being a Wallflower":"Gay","Vice":"Peripheral","Onward":"Peripheral","The Intouchables":"Peripheral","Booksmart":"Lesbian","Suspiria":"Peripheral","The Mule":"Peripheral","The World's End":"Peripheral","The Neon Demon":"Peripheral","Sausage Party":"Peripheral","A Million Ways to Die in the West":"Peripheral","Happy Death Day":"Peripheral","Power Rangers":"Peripheral","The Invitation":"Peripheral","Love, Simon":"Gay","Dallas Buyers Club":"Transgender","Bombshell":"Lesbian"}
 
 const panel = document.getElementById('panel');
 
@@ -27,33 +27,34 @@ for (i = 0; i < 50; i++){
 // test
 const test = document.getElementById('test');
 
-const movies = {
-  movie1:"I care a lot"
-}
 
-console.log(movies[0])
-// for (i=0; i<2; i++){
-//   console.log(movies[i])
+// for (let key in movies){
+//   console.log(key)
 // }
 
-for (i = 0; i < 50; i++){
-    let div1 = document.createElement('div');
-    div1.className = 'flip-card';
-    // div1.className = 'card';
-    let div2 = document.createElement("div")
-    div2.className = 'flip-card-inner'
-    let div3a = document.createElement("div")
-    div3a.className = 'flip-card-front'
-    // div3a.innerHTML = "FRONT"
-    // div3a.innerHTML += i
-    let div3b = document.createElement("div")
-    div3b.className = 'flip-card-back'
-    div3b.innerHTML = "BACK"
-    // childDiv.innerHTML = i
-    div2.appendChild(div3a)
-    div2.appendChild(div3b)
-    div1.appendChild(div2)
-    test.appendChild(div1);
+// const {jsonData}= require('./assets/movies.json'); 
+// console.log(jsonData);
+
+
+for (let movie in movies){
+  let div1 = document.createElement('div');
+  div1.className = 'flip-card';
+  // div1.className = 'card';
+  let div2 = document.createElement("div")
+  div2.className = 'flip-card-inner'
+  let div3a = document.createElement("div")
+  div3a.className = 'flip-card-front'
+  div3a.innerHTML = movie
+  // div3a.innerHTML += i
+  let div3b = document.createElement("div")
+  div3b.className = 'flip-card-back'
+  div3b.classList.add(movies[movie])
+  div3b.innerHTML = movies[movie]
+  // childDiv.innerHTML = i
+  div2.appendChild(div3a)
+  div2.appendChild(div3b)
+  div1.appendChild(div2)
+  test.appendChild(div1);
 }
 
 /* <div class="flip-card">
