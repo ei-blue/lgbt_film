29d7f65c8cde54fc35006eac35c1d67987bff3b7
Ah-oy <58558581+ei-blue@users.noreply.github.com>
2022-05-07 16:48:27 -0700

    adding margin to each page

--- script.js
+++ script.js
@@ -1,40 +1,32 @@
-const movies = {"Captain Marvel":"Peripheral","I Care a Lot":"Lesbian","Bohemian Rhapsody":"Gay","A Star Is Born":"Peripheral","Solo: A Star Wars Story":"Peripheral","Bird Box":"Peripheral","Birds of Prey":"Bisexual","Blade Runner 2049":"Peripheral","It Chapter Two":"Peripheral","Alien: Covenant":"Peripheral","Get Out":"Peripheral","Guardians of the Galaxy":"Peripheral","Deadpool":"Peripheral","Green Book":"Gay","The Old Guard":"Peripheral","Fantastic Beasts: The Crimes of Grindelwald":"Peripheral","Deadpool 2":"Peripheral","Beauty and the Beast":"Peripheral","The Shape of Water":"Gay","Guardians of the Galaxy Vol. 2":"Peripheral","The Favourite":"Lesbian","The Grand Budapest Hotel":"Peripheral","Ghost in the Shell":"Peripheral","Call Me by Your Name":"Gay","The New Mutants":"Peripheral","Moonlight":"Gay","Star Trek Beyond":"Peripheral","The Imitation Game":"Gay","Atomic Blonde":"Bisexual","The Lobster":"Peripheral","A Simple Favor":"Bisexual","Spotlight":"Gay","Ted":"Peripheral","The Perks of Being a Wallflower":"Gay","Vice":"Peripheral","Onward":"Peripheral","The Intouchables":"Peripheral","Booksmart":"Lesbian","Suspiria":"Peripheral","The Mule":"Peripheral","The World's End":"Peripheral","The Neon Demon":"Peripheral","Sausage Party":"Peripheral","A Million Ways to Die in the West":"Peripheral","Happy Death Day":"Peripheral","Power Rangers":"Peripheral","The Invitation":"Peripheral","Love, Simon":"Gay","Dallas Buyers Club":"Transgender","Bombshell":"Lesbian"}
+const movies = {"Captain Marvel":"Peripheral",
+"I Care a Lot":"Lesbian","Bohemian Rhapsody":"Gay","A Star Is Born":"Peripheral",
+"Solo: A Star Wars Story":"Peripheral","Bird Box":"Peripheral","Birds of Prey":"Bisexual",
+"Blade Runner 2049":"Peripheral","It Chapter Two":"Peripheral","Alien: Covenant":"Peripheral","Get Out":"Peripheral",
+"Guardians of the Galaxy":"Peripheral","Deadpool":"Peripheral","Green Book":"Gay","The Old Guard":"Peripheral",
+"Fantastic Beasts: The Crimes of Grindelwald":"Peripheral","Deadpool 2":"Peripheral",
+"Beauty and the Beast":"Peripheral","The Shape of Water":"Gay","Guardians of the Galaxy Vol. 2":"Peripheral",
+"The Favourite":"Lesbian","The Grand Budapest Hotel":"Peripheral","Ghost in the Shell":"Peripheral",
+"Call Me by Your Name":"Gay","The New Mutants":"Peripheral","Moonlight":"Gay","Star Trek Beyond":"Peripheral",
+"The Imitation Game":"Gay","Atomic Blonde":"Bisexual","The Lobster":"Peripheral","A Simple Favor":"Bisexual",
+"Spotlight":"Gay","Ted":"Peripheral","The Perks of Being a Wallflower":"Gay","Vice":"Peripheral",
+"Onward":"Peripheral","The Intouchables":"Peripheral","Booksmart":"Lesbian","Suspiria":"Peripheral",
+"The Mule":"Peripheral","The World's End":"Peripheral","The Neon Demon":"Peripheral","Sausage Party":"Peripheral",
+"A Million Ways to Die in the West":"Peripheral","Happy Death Day":"Peripheral","Power Rangers":"Peripheral",
+"The Invitation":"Peripheral","Love, Simon":"Gay","Dallas Buyers Club":"Transgender","Bombshell":"Lesbian"}
 
-const panel = document.getElementById('panel');
+// const panel = document.getElementById('panel');
 
 // div
-for (i = 0; i < 50; i++){
-    let div = document.createElement('div');
-    div.className = 'card';
-    div.innerHTML = '';
-    panel.appendChild(div);
-}
-
-// const interactive = document.getElementById('interactive');
-
-// for (i = 0; i < 3; i++){
-//     let parentDiv = document.createElement('div');
-//     // div.className = 'card card-flip';
-//     parentDiv.className = 'container1';
-//     parentDiv.innerHTML = "";
-//     let childDiv = document.createElement("div")
-//     childDiv.className = 'card target'
-//     childDiv.innerHTML = i
-//     parentDiv.appendChild(childDiv)
-//     interactive.appendChild(parentDiv);
+// for (i = 0; i < 50; i++){
+//     let div = document.createElement('div');
+//     div.className = 'card';
+//     div.innerHTML = '';
+//     panel.appendChild(div);
 // }
+
 
 // test
 const test = document.getElementById('test');
-
-
-// for (let key in movies){
-//   console.log(key)
-// }
-
-// const {jsonData}= require('./assets/movies.json'); 
-// console.log(jsonData);
-
 
 for (let movie in movies){
   let div1 = document.createElement('div');
@@ -57,37 +49,3 @@ for (let movie in movies){
   test.appendChild(div1);
 }
 
-/* <div class="flip-card">
-  <div class="flip-card-inner">
-    <div class="flip-card-front">
-      <h1>TEST</h1>
-    </div>
-    <div class="flip-card-back"></div> */
-
-
-// div
-// for (i = 0; i < 5; i++){
-//     let div = document.createElement('div');
-//     // div.className = 'card card-flip';
-//     div.className = 'card target';
-//     div.innerHTML = i;
-//     interactive.appendChild(div);
-// }
-
-
-
-// window.onload = function(){
-//     let panel = document.getElementById("panel")
-//     for (i=0; i < 10; i++) {
-//         let div = document.createElement("div")
-//         div.className = "card back"
-//         div.index = div.innerHTML = ""
-//         panel.appendChild(div)
-//     }
-// }
-
-// for (i=0; i<5; i++) {
-//     // let div = document.createElement("div")
-//     panel.appendChild(div)
-//     // console.log(i)
-// }
